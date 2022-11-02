f9f6bc9a17f8a40cf3d168d85b7f72ca1547fe4e
SuhaniPandey <iamsuhani5@gmail.com>
2022-11-02 11:27:04 +0100

    Junit

--- Assignment_2/src/client/views/ViewController.java
+++
@@ -1,9 +0,0 @@
-package client.views;
-
-import client.core.ViewHandler;
-import client.core.ViewModelFactory;
-
-public interface ViewController
-{
-  void init(ViewHandler viewHandler, ViewModelFactory viewModelFactory);
-}
---
+++ UnitTesting/test/clock/ClockTest.java
@@ -0,0 +1,47 @@
+package clock;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClockTest
+{
+
+  @Before public void setUp() throws Exception
+  {
+  }
+
+  @Test public void tic()
+  {
+  }
+
+  @Test public void getHour()
+  {
+  }
+
+  @Test public void getMinute()
+  {
+  }
+
+  @Test public void getSecond()
+  {
+  }
+
+  @Test public void set()
+  {
+
+  }
+
+  @Test public void testSet()
+  {
+  }
+
+  @Test public void getTimeInSeconds()
+  {
+  }
+
+  @Test public void testToString()
+  {
+  }
+}
